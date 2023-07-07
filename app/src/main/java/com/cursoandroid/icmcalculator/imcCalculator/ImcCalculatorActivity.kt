f63712e32c1ca2d97356ee053b1b277154f57e64
pablo.martin <pablo.martin@mad.vass.es>
2023-07-07 18:46:41 +0200

    Primer Commit

---
+++ app/src/main/java/com/cursoandroid/icmcalculator/imcCalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.cursoandroid.androidmaster.imcCalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cursoandroid.icmcalculator.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
