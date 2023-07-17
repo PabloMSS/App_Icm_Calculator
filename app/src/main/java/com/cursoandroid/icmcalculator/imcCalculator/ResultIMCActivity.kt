95e05517b69225cbf8179da21d8a682228c45c97
pablo.martin <pablomartinsanchezseco02@gmail.com>
2023-07-17 16:26:45 +0200

    Desarrollo FInal

--- app/src/main/java/com/cursoandroid/icmcalculator/imcCalculator/ResultIMCActivity.kt
+++ app/src/main/java/com/cursoandroid/icmcalculator/imcCalculator/ResultIMCActivity.kt
@@ -3,8 +3,10 @@ package com.cursoandroid.icmcalculator.imcCalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.cursoandroid.androidmaster.imcCalculator.ImcCalculatorActivity
 import com.cursoandroid.icmcalculator.R
 
@@ -36,7 +38,7 @@ class ResultIMCActivity : AppCompatActivity() {
         iniUI(resultIMCFInal)
 
         btnRecalculate.setOnClickListener {
-            onBackPressed()
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 
@@ -45,18 +47,22 @@ class ResultIMCActivity : AppCompatActivity() {
         when(result){
             in 0.0 .. 18.50 -> {
                 tvResult.text = getString(R.string.title_peso_bajo)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.text_no_normal))
                 tvDescription.text = getString(R.string.descripcion_peso_bajo)
             }
             in 18.51 .. 24.99 -> {
                 tvResult.text = getString(R.string.title_peso_normal)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.text_normal))
                 tvDescription.text = getString(R.string.descripcion_peso_normal)
             }
             in 25.00 .. 29.99 -> {
                 tvResult.text = getString(R.string.title_sobrepeso)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.text_bad))
                 tvDescription.text = getString(R.string.descripcion_sobrepeso)
             }
             in 30.00 .. 99.00 -> {
                 tvResult.text = getString(R.string.title_obesidad)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.text_ultra_bad))
                 tvDescription.text = getString(R.string.descripcion_obesidad)
             }
             else -> {
